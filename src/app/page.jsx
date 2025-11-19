9220bce38ddc9d29a51a112d0169fef4a61012d9
adriaxy <adrisup959@gmail.com>
2025-11-19 18:49:01 +0100

    Add initial page for vERCEL BUILD

---
+++ src/app/page.jsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+        <main>
+            <h1>CSS GAME</h1>
+            <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit a exercitationem veritatis.</P>
+        </main>
+    )
+}
