66b354523fb9a388104b27bd202636efa13d26b4
adriaxy <adrisup959@gmail.com>
2025-11-19 18:51:47 +0100

    Fix code syntax

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@ export default function HomePage() {
     return (
         <main>
             <h1>CSS GAME</h1>
-            <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit a exercitationem veritatis.</P>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit a exercitationem veritatis.</p>
         </main>
     )
 }
