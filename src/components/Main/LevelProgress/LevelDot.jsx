ecb47d630b097678a722f1b50804caaa72080e44
adriaxy <adrisup959@gmail.com>
2025-12-07 15:03:09 +0100

    Feat: dot progress now show sublevel name when hovered

--- src/components/Main/LevelProgress/LevelDot.jsx
+++ src/components/Main/LevelProgress/LevelDot.jsx
@@ -1,8 +1,18 @@
 import { useEffect, useState } from 'react';
 import './LevelDot.css';
 
-export default function LevelDot({level, sublevel, isCompleted}) {
+
+
+export default function LevelDot({level, sublevel, isCompleted, dotName}) {
+    let [isHovered, setIsHovered] = useState('')
+    
     return(
-        <button className={`level-dot ${isCompleted}`}/>
+        <div className="level-dot-wrapper" onMouseEnter={() => setIsHovered('show')} onMouseLeave={() => setIsHovered('')}>
+            <button className={`level-dot ${isCompleted}`} />
+            <span className={`level-dot__sublevel-name ${isHovered}`}>
+                {dotName}
+                <span className='level-dot__sublevel-name__edge'></span>
+            </span>
+        </div>
     )
 }
--- src/components/Main/LevelProgress/ProgressDots.jsx
+++ src/components/Main/LevelProgress/ProgressDots.jsx
@@ -1,6 +1,7 @@
 import './ProgressDots.css';
 import LevelDot from './LevelDot';
 import { useGame } from '@/app/GameContext';
+import levels from '@/data/levels';
 
 export default function ProgressDots({numOfSublevels, level, sublevel}) {
     const { sublevelState, currentLevel, currentSublevel } = useGame();
@@ -8,7 +9,7 @@ export default function ProgressDots({numOfSublevels, level, sublevel}) {
     return(
         <div className='progress-dots'>
             {Array.from({ length: numOfSublevels }).map((_, i) => (
-                <LevelDot key={i} level={level} sublevel={i} isCompleted={sublevelState[currentLevel][i]?.completed ? 'completed' : ''}/>
+                <LevelDot key={i} level={level} sublevel={i} dotName={levels?.[currentLevel]?.sublevels[i]?.name} isCompleted={sublevelState[currentLevel][i]?.completed ? 'completed' : ''}/>
             ))}
         </div>
     )
