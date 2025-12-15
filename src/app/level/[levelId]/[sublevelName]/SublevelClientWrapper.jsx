7f801b66589fd285190e6109fa9ef84ecbc0d740
adriaxy <adrisup959@gmail.com>
2025-12-15 18:34:32 +0100

    Add max width on general layout

--- src/app/level/[levelId]/[sublevelName]/SublevelClientWrapper.jsx
+++ src/app/level/[levelId]/[sublevelName]/SublevelClientWrapper.jsx
@@ -16,23 +16,3 @@ export default function SublevelClientWrapper({ levelId, sublevelName, children 
 
   return children;
 }
-
-
-// "use client";
-// import { useEffect, useState } from "react";
-// import { useGame } from "@/app/GameContext";
-// import levels from "@/data/levels";
-
-// export default function SublevelClientWrapper({ levelId, sublevelName, children }) {
-//   const { setCurrentLevel, setCurrentSublevel, currentLevel, currentSublevel } = useGame();
-
-//   useEffect(() => {
-//     const levelIndex = levels.findIndex(l => l.level === Number(levelId));
-//     const sublevelIndex = levels[levelIndex].sublevels.findIndex(s => s.name === sublevelName);
-
-//     setCurrentLevel(levelIndex);
-//     setCurrentSublevel(sublevelIndex);
-//   }, [levelId, sublevelName]);
-
-//   return children;
-// }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,21 +11,23 @@ export default function Header({ onPrev, onNext, isPrevDisabled, isNextDisabled}
     const sublevelName = currentSublevelData.name.toUpperCase();
 
     return (
-        <header className="header">
-            <LogoButton />
-            <PrevChallengeButton onClick={onPrev} isPrevDisabled={isPrevDisabled}/>
-            <div className="header__center">
-                {<h1 className='header__title'>Level {currentLevelData.level} : <span>{currentLevelData.name}</span></h1>}
-            <div className="header__title-outline"></div>
-            </div>
-            <NextChallengeButton onClick={onNext} isNextDisabled={isNextDisabled}/>
-            <div className="header__right">
-                <BackHomeButton
-                    className={"header__home-btn"}
-                    type={"home-btn"}
-                />
-                <ThemeModeButton />
-            </div>
-        </header>
+        <div className='header_container'>
+            <header className="header">
+                <LogoButton />
+                <PrevChallengeButton onClick={onPrev} isPrevDisabled={isPrevDisabled}/>
+                <div className="header__center">
+                    {<h1 className='header__title'>Level {currentLevelData.level} : <span>{currentLevelData.name}</span></h1>}
+                <div className="header__title-outline"></div>
+                </div>
+                <NextChallengeButton onClick={onNext} isNextDisabled={isNextDisabled}/>
+                <div className="header__right">
+                    <BackHomeButton
+                        className={"header__home-btn"}
+                        type={"home-btn"}
+                    />
+                    <ThemeModeButton />
+                </div>
+            </header>
+        </div>
     )
 }
