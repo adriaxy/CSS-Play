433cad6070a6b2739733362e1cf0649648b13c19
adriaxy <adrisup959@gmail.com>
2025-12-17 19:17:57 +0100

    Logo updated

--- src/components/Header/LogoButton.jsx
+++ src/components/Header/LogoButton.jsx
@@ -2,7 +2,38 @@ export default function LogoButton(){
     return (
         <div className='header__left'>
                 <div className='header__logo'>
-                    <span className='header__logo-bold'>CSS</span> <span className="header__logo-light">Play</span>
+                    <div className="header__text"><span className='header__logo-bold'>CSS</span> <span className="header__logo-light">Play</span></div> 
+                    <svg
+                        version="1.1"
+                        id="Capa_1"
+                        xmlns="http://www.w3.org/2000/svg"
+                        xmlnsXlink="http://www.w3.org/1999/xlink"
+                        x="0px"
+                        y="0px"
+                        viewBox="0 0 15.1 13"
+                        style={{ enableBackground: 'new 0 0 15.1 13' }}
+                        xmlSpace="preserve"
+                        className="header__logo-svg"
+                    >
+                        <style type="text/css">
+                        {`
+                            .st0{clip-path:url(#SVGID_2_);fill:#0D1F33;}
+                            .st1{clip-path:url(#SVGID_2_);fill:#FFFFFF;}
+                            .st2{clip-path:url(#SVGID_2_);fill:#FFC929;}
+                        `}
+                        </style>
+                        <g>
+                        <defs>
+                            <rect id="SVGID_1_" width="15.1" height="13" />
+                        </defs>
+                        <clipPath id="SVGID_2_">
+                            <use xlinkHref="#SVGID_1_" style={{ overflow: 'visible' }} />
+                        </clipPath>
+                        <path className="st0" d="M1.6,0C1.6,0,0,0,0,1.6v9.8c0,0,0,1.6,1.6,1.6h11.8c0,0,1.6,0,1.6-1.6V1.6c0,0,0-1.6-1.6-1.6H1.6z" />
+                        <path className="st1" d="M4.4,6.3c0,0-1.6,0-1.6,1.6v2.2c0,0,0,1.6,1.6,1.6h0.4c0,0,1.6,0,1.6-1.6V7.9c0,0,0-1.6-1.6-1.6H4.4z" />
+                        <path className="st2" d="M10.3,2.2c0,0-1.6,0-1.6,1.6v6.3c0,0,0,1.6,1.6,1.6h0.4c0,0,1.6,0,1.6-1.6V3.8c0,0,0-1.6-1.6-1.6H10.3z" />
+                        </g>
+                    </svg>
                 </div>
         </div>
     )
